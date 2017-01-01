e012b819e7008163febf0f95b8f134e444dda7e9
Rauf E <rawoof@hotmail.com>
2017-01-01 21:42:03 +0000

    Fixed ContactMailer class

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,9 @@
 class ContactMailer < ActionMailer::Base
-    default to 'somalasol11@gmail.com'
+    default to: 'somalasol11@gmail.com'
     
-    def contact_email(name , email , body)
-        @name = name
+    def contact_email(fname , lname, email , body)
+        @fname = fname
+        @lname = lname
         @email = email
         @body = body
         
