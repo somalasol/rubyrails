105ed6e74d551e34d43e997ce42ef293e2806e63
Rauf E <rawoof@hotmail.com>
2017-01-01 20:50:26 +0000

    Added contact mailer functionality

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,18 @@ class ContactsController < ApplicationController
    def create
       @contact = Contact.new(contact_params)
        if @contact.save
-     flash[:success] = 'Message sent'
-     redirect_to new_contact_path
-      else
-     flash[:danger] = @contact.errors.full_messages.join(', ')
-      redirect_to new_contact_path
+        fname = params [:contact][:fname]
+        lname = params [:contact][:lname]
+        email = params [:contact][:email]
+        body = params [:contact][:comments]
+        
+        ContactMailer.contact_email(fname , lname , email , body).deliver
+         flash[:success] = 'Message sent'
+         redirect_to new_contact_path
+          
+          else
+         flash[:danger] = @contact.errors.full_messages.join(', ')
+          redirect_to new_contact_path
   
       end
    end
